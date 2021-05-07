d678ba4c357df97d57d8ad20c534ff3b67581fc3
liandre.p.de.castro <liandre.p.de.castro@accenture.com>
2021-05-07 11:53:48 +0800

    set-up initial module

---
+++ src/main/java/com/lpdecastro/YelpReviewsApp.java
@@ -0,0 +1,18 @@
+package com.lpdecastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liandre.p.de.castro
+ *
+ * @since 2021/05/07
+ */
+@SpringBootApplication
+public class YelpReviewsApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YelpReviewsApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/lpdecastro/YelpReviewsAppTests.java
@@ -0,0 +1,13 @@
+package com.lpdecastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YelpReviewsAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
